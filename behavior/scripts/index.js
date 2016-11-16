a66baeeb2f01e0dd234d9f62c7c1526f2bfd6101
viking2917 <viking2917@gmail.com>
2016-11-16 17:37:57 -0500

    more paths

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -38,6 +38,21 @@ exports.handle = function handle(client) {
     // 	}
     // })
     
+
+    const sendGreeting = client.createStep({
+	satisfied() {
+	    return Boolean(client.getConversationState().greetingSent)
+	},
+	
+	prompt() {
+	    client.addResponse('app:response:name:welcome')
+	    client.updateConversationState({
+		greetingSent: true
+	    })
+	    client.done()
+	}
+    })
+
     const provideHelp = client.createStep({
 	satisfied() {
 	    return Boolean(client.getConversationState().helpSent)
@@ -228,107 +243,32 @@ exports.handle = function handle(client) {
 	prompt(callback) {
 	    console.log('extracting slots')
 
-	    var bookTitle = firstOfEntityRole(client.getMessagePart(), 'booktitle')
-	    if(!bookTitle) bookTitle = ""
-	    else bookTitle = bookTitle.value
-	    console.log('Title: ' + bookTitle)
-	    var bookAuthor = firstOfEntityRole(client.getMessagePart(), 'authorname')
-	    if(!bookAuthor) bookAuthor = ""
-	    else bookAuthor = bookAuthor.value
-	    console.log('Author: ' + bookAuthor)
+	    var interest1 = firstOfEntityRole(client.getMessagePart(), 'interest1')
+	    if(!interest1) interest1 = ""
+	    else interest1 = interest1.value
+	    console.log('interest1: ' + interest1)
 
-	    getSimilar(bookTitle, bookAuthor, resultBody => {
-		if (!resultBody) {
-		    console.log('Error getting trending book.')
-		    client.addResponse('app:response:name:apology/untrained')
-		    client.done()
-		    callback()
-		    return
-		}
+	    var interest2 = firstOfEntityRole(client.getMessagePart(), 'interest2')
+	    if(!interest2) interest2 = ""
+	    else interest2 = interest2.value
+	    console.log('interest2: ' + interest2)
 
-		const theBook = resultBody;
-		setClientCache.recordBookRead(client, theBook)
+	    var interest3 = firstOfEntityRole(client.getMessagePart(), 'interest3')
+	    if(!interest3) interest3 = ""
+	    else interest3 = interest3.value
+	    console.log('interest3: ' + interest3)
 
-		const relBook1 = resultBody.relatedbooks[0];
-		const relBook2 = resultBody.relatedbooks[1];
+	    if(interest3) 
+		client.addTextResponse('(I think you said <' + interest1 + '>, <' + interest2 + '>, <' + interest3 + '>.')
+	    else if (interest2) 
+		client.addTextResponse('(I think you said <' + interest1 + '>, <' + interest2 + '>.')
+	    else if (interest1) 
+		client.addTextResponse('(I think you said <' + interest1 + '>.')
 
-		const shortdesc1 = striptags(relBook1.description).substring(0,  50) + "..."
-		const shortdesc2 = striptags(relBook2.description).substring(0, 50) + "..."
-
-		console.log(relBook1)
-		const bookData1 = {
-		    BookTitle: relBook1.title,
-		    AuthorName: relBook1.authorstring,
-		    BookLink: 'https://www.thehawaiiproject.com/' + urlTools.book_url(relBook1.title,relBook1.authorstring,relBook1.bookid),
-		}
-		const bookData2 = {
-		    BookTitle: relBook2.title,
-		    AuthorName: relBook2.authorstring,
-		    BookLink: 'https://www.thehawaiiproject.com/' + urlTools.book_url(relBook2.title,relBook2.authorstring,relBook2.bookid),
-		}
-
-		console.log('sending book data:', bookData1)
-		console.log('sending book data:', bookData2)
-		setClientCache.recordBookSent(client, relBook1)
-		setClientCache.recordBookSent(client, relBook2)
-		// client.addTextResponse('(I think you said ' + bookTitle + ' by ' + bookAuthor + '.)')
-		client.addTextResponse('(I think you typed a title of <' + bookTitle + '> and an author of <' + bookAuthor + '> so I assume you meant ' + resultBody.title + ' by ' + resultBody.authorstring + '.)')
-		client.addResponse('app:response:name:provide_response_recommendation', bookData1)
-		client.addImageResponse( relBook1.coverarturl, 'The product')
-
-		client.addCarouselListResponse({
-		    items: [
-			{
-			    'media_url': relBook1.coverarturl,
-			    'media_type': 'image/jpeg', 
-			    'description': shortdesc1,
-			    title: relBook1.title.substring(0,78),
-			    actions: [
-				{
-				    type: 'link',
-				    text: 'See More',
-				    uri: bookData1.BookLink,
-				},
-			    ],
-			},
-			{
-			    'media_url': relBook2.coverarturl,
-			    'media_type': 'image/jpeg', 
-			    'description': shortdesc2,
-			    title: relBook2.title.substring(0,78),
-			    actions: [
-				{
-				    type: 'link',
-				    text: 'See More',
-				    uri: bookData2.BookLink,
-				},
-			    ],
-			},
-		    ],
-		})
-
-
-		client.done()
-		callback()
-	    })
-	},
-    })
-
-
-    const sendGreeting = client.createStep({
-	satisfied() {
-	    return Boolean(client.getConversationState().greetingSent)
-	},
-	
-	prompt() {
-	    client.addResponse('app:response:name:welcome')
-	    client.updateConversationState({
-		greetingSent: true
-		})
 	    client.done()
-	}
+	},
     })
-    
+
     const askBook = client.createStep({
 	satisfied() {
 	    return false
@@ -342,7 +282,7 @@ exports.handle = function handle(client) {
 	}
     })
     
-    const askInterests = client.createStep({
+    const askIfGift = client.createStep({
 	satisfied() {
 	    return false
 	},
@@ -353,6 +293,62 @@ exports.handle = function handle(client) {
 	    // client.expect('liked_book', ['decline', 'similar1'])  // these are streams, not message classifications.
 	    client.done()
 	}
+    })
+
+    const collectInterests = client.createStep({
+	satisfied() {
+	    //return Boolean(client.getConversationState().weatherCity)
+	    return false
+	},
+	
+	extractInfo() {
+	    
+	    console.log('extracting slots')
+	    
+	    var interest1 = firstOfEntityRole(client.getMessagePart(), 'interest1')
+	    if(!interest1) interest1 = ""
+	    else { 
+		interest1 = interest1.value
+		console.log('interest1: ' + interest1)
+		client.updateConversationState({
+		    interest1: interest1,
+		})
+	    }
+
+	    var interest2 = firstOfEntityRole(client.getMessagePart(), 'interest2')
+	    if(!interest2) interest2 = ""
+	    else {
+		interest2 = interest2.value
+		console.log('interest2: ' + interest2)
+		client.updateConversationState({
+		    interest2: interest2,
+		})
+	    }
+
+	    var interest3 = firstOfEntityRole(client.getMessagePart(), 'interest3')
+	    if(!interest3) interest3 = ""
+	    else { 
+		interest3 = interest3.value
+		console.log('interest3: ' + interest3)
+		client.updateConversationState({
+		    interest3: interest3,
+		})		
+	    }
+
+	    // if(interest3) 
+	    // 	client.addTextResponse('(I think you said <' + interest1 + '>, <' + interest2 + '>, <' + interest3 + '>.')
+	    // else if (interest2) 
+	    // 	client.addTextResponse('(I think you said <' + interest1 + '>, <' + interest2 + '>.')
+	    // else if (interest1) 
+	    // 	client.addTextResponse('(I think you said <' + interest1 + '>.')
+
+	    client.done()
+	},
+	
+	prompt() {
+	    client.addResponse('app:response:name:prompt/weather_city')
+	    client.done()
+	},
     })
     
     const rejectReco = client.createStep({
@@ -392,7 +388,8 @@ exports.handle = function handle(client) {
 	    greeting: 'greetingStream',
 	    goodbye: 'goodbyeStream',
 	    ask_trending_book: 'trendingStream',
-	    looking_for_gift: 'similarStream',
+// 	    looking_for_gift: 'askForInterests',
+	    // looking_for_gift: 'provideBookonInterests',
 	    liked_book: 'similarStream',
 	    request_for_help: 'helpStream',
 	    turing: 'turingStream',
@@ -403,7 +400,7 @@ exports.handle = function handle(client) {
 	    //provide_popular_book: 'getTrending',
 	},
 	streams: {
-	    greetingStream: [askInterests],
+	    greetingStream: [askIfGift, collectInterests, provideBookonInterests],
 	    goodbyeStream: handleGoodbye,
 	    turingStream: handleTuring,
 	    trendingStream: provideTrendingBook,
